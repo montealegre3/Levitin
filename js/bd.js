2901b7d25cb3476fd018498a122b846546366d78
montealegre3 <julianandrea.silva@gmail.com>
2024-08-28 16:47:04 -0500

    Levitin

---
+++ js/bd.js
@@ -0,0 +1,82 @@
+let modulos = [
+    {
+        id: 1,
+        titulo: 'Módulo 1',
+        subtitulo1: 'Introducción a la psicología musical',
+        descripcion: 'Abarcaremos tanto la historia y evolución, como la importancia y prácticas de la psicología musical',
+        temas: [
+            {
+                titulo: 'Introducción a la psicología musical',
+                descripcion: 'A lo largo de la historia la música ha acompañado al ser humano, desde tiempos inmemorables los sonidos transmiten emociones y han sido una forma de expresión humana, lingüística y cultural. Durante años la música ha sido objeto de estudio, la humanidad ha explorado su relación con la mente y es ahí cuando aparece la psicología, la ciencia que estudia los procesos mentales.',
+                video: '',
+            },
+
+            {
+                titulo: '¿Qué es la psicología musical?',
+                descripcion: 'La psicología musical es un campo interdisciplinario de la psicología que estudia cómo las personas perciben, comprenden, experimentan y se relacionan con la música, así como los efectos de la música en la mente, el cuerpo y el comportamiento humano.',
+                video: '',
+            },
+
+            {
+                titulo: 'Historia y evolución de la psicología musical',
+                descripcion: 'En la antigüedad, se creía frmemente en el poder de la música para infuir en la salud física, mental y espiritual de las personas. En muchas cultras antiguas como la egipcia, la griega, la china e incluso la mesopotámica, se utilizaba la música como una herramienta terapéutica para promover la curación y el bienestar.',
+                video: '',
+            },
+
+            {
+                titulo: 'Importacia y aplicaciones prácticas de la psicología musical',
+                descripcion: 'La psicología musical ayuda a comprender cómo la música afecta la mente y el comportamiento humano, y cómo podemos utilizar este conocimiento de manera práctica en diversas áreas, desde la salud y el bienestar hasta la educación y el entretenimiento.',
+                video: '',
+               
+            },
+        ]
+    },
+
+    {
+        id: 2,
+        titulo: 'Módulo 2',
+        subtitulo1: 'Percepción musical',
+        descripcion: 'Abarcaremos desde los elementos de la música (tono, ritmo, melodía y armonía) hasta el procesamiento auditivo',
+        temas: [
+            {
+                titulo: 'Percepción musical',
+                descripcion: 'La percepción musical es la capacidad de interpretar y comprender la música a través de los sentidos, especialmente el oído. Esta área también implica la capacidad de reconocer patrones musicales, identifcar instrumentos y comprender el signifcado emocional y expresivo de la música. ',
+                video: '',
+            },
+
+        ]
+    },
+
+    {
+        id: 3,
+        titulo: 'Módulo 3',
+        subtitulo1: 'Música y emociones',
+        descripcion: 'Nos adentramos en la musicoterapia y en cómo la música puede evocar sentimientos',
+        temas: [
+            {
+                titulo: 'La música y las emociones: Cómo la música puede evocar sentimientos',
+                descripcion: '',
+                video: '',
+            },
+
+            {
+                titulo: 'Musicoterapia',
+                descripcion: '',
+                video: '',
+            }
+        ]
+    },
+
+    {
+        id: 4,
+        titulo: 'Módulo 4',
+        subtitulo1: 'Música y memoria',
+        descripcion: ' Abarcaremos el efecto de la música en la memoria y la relación entre la música, la nostalgia y los recuerdos emocionales. ',
+    }
+
+
+
+
+]
+
+
