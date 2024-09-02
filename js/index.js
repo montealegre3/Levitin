d6e3201fe9f46e1ad1e85356a9aa6adacb0af30b
montealegre3 <julianandrea.silva@gmail.com>
2024-09-02 14:53:26 -0500

    Levitin

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ const saludo = document.querySelector('#saludo');
 
 let nombreUsuario = JSON.parse(localStorage.getItem('user'));
 
-saludo.innerHTML = `Hola ${nombreUsuario.userName} 🎶🎶`
+saludo.innerHTML = `Hola ${saludo.userName} 🎶🎶`
 
 
 const btnIniciarSesion = document.querySelector('#btnIniciarSesion');
 const btnRegistro = document.querySelector('#btnRegistro');
 const saludoUsuario = document.querySelector('#saludo');
+const cerrarSesion = document.querySelector('#cerrar');
 
 
 
@@ -16,20 +17,29 @@ function verificarSesion() {
     let currentUser = JSON.parse(localStorage.getItem('user'));
 
     if (currentUser) {
-        // Si hay un usuario logueado, mostrar su nombre y ocultar botones
+        // Si hay un usuario logueado, mostrar su nombre, ocultar botones inicio de sesion y mostrar boton cerrar sesión
         saludoUsuario.textContent = `Bienvenido, ${currentUser.userName} 🎶🎶`;
         btnIniciarSesion.style.display = 'none';
         btnRegistro.style.display = 'none';
+        cerrarSesion.style.display = 'inline-block'
     } else {
-        // Si no hay usuario logueado, mostrar los botones y ocultar el nombre
+        // Si no hay usuario logueado, mostrar los botones, ocultar el nombre y el boton de cerrar sesion
         saludoUsuario.textContent = '';
         btnIniciarSesion.style.display = 'inline-block';
         btnRegistro.style.display = 'inline-block';
+        cerrarSesion.style.display = 'none';
     }
 }
 
 
+cerrarSesion.addEventListener('click' , () => {
+    localStorage.removeItem('user');
+    verificarSesion();
+})
+
 document.addEventListener('DOMContentLoaded', verificarSesion);
+
+
 
 
 
