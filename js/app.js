9520c99de805ac3acc94199a5ec5381e18f82872
montealegre3 <julianandrea.silva@gmail.com>
2025-02-20 16:11:26 -0500

    menu desplegable

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//Código para la funcionalidad del menu dropwdom
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".dropdown-submenu .dropdown-toggle").forEach(function (element) {
+      element.addEventListener("click", function (e) {
+        e.preventDefault(); // Evita que la página recargue
+        e.stopPropagation(); // Evita que el menú principal se cierre
+
+        let submenu = this.nextElementSibling;
+        if (submenu.classList.contains("show")) {
+          submenu.classList.remove("show");
+        } else {
+          document.querySelectorAll(".dropdown-submenu .dropdown-menu").forEach(function (el) {
+            el.classList.remove("show");
+          });
+          submenu.classList.add("show");
+        }
+      });
+    });
+
+    // Cierra el submenú si se hace clic fuera de él
+    document.addEventListener("click", function () {
+      document.querySelectorAll(".dropdown-submenu .dropdown-menu").forEach(function (el) {
+        el.classList.remove("show");
+      });
+    });
+});
