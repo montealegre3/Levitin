ef89acdc7939f8ff840f7c822fb85d9826360c84
montealegre3 <julianandrea.silva@gmail.com>
2025-04-03 16:15:23 -0500

    modificación estilos header

--- js/modulo1/tema1.js
+++ js/modulo1/tema1.js
@@ -3,25 +3,6 @@ import { validarSesion , mostrarProgreso } from "../validarSesion.js";
 validarSesion()
 mostrarProgreso()
 
-/* document.addEventListener("DOMContentLoaded", () => {
-    const sorprendido = document.querySelector(".beethoven.sorprendido");
-    const feliz = document.querySelector(".beethoven.feliz");
-  
-    // Muestra la imagen inicial
-    sorprendido.style.opacity = 1;
-  
-    // Alterna entre las imágenes
-    setInterval(() => {
-      if (sorprendido.style.opacity == 1) {
-        sorprendido.style.opacity = 0;
-        feliz.style.opacity = 1;
-      } else {
-        sorprendido.style.opacity = 1;
-        feliz.style.opacity = 0;
-      }
-    }, 1000); //Tiempo de duración 
-  }); */
-
 //Código para la funcionalidad del menu dropwdom
 
 document.addEventListener("DOMContentLoaded", function () {
