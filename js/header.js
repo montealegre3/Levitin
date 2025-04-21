45849ae81776f5170fb8c32964bb09539b32b360
IsabellaBarrero <isabella.barrero.93@gmail.com>
2025-04-21 15:20:18 -0500

    rutas

--- js/header.js
+++ js/header.js
@@ -247,19 +247,19 @@ function header() {
               <li class="mb-3">
                   <h6>Módulos aprobados</h6>
                   <div id="modulo1" class="d-flex justify-content-between align-items-center">
-                      <a href="./vistas/modulo1/animacion1.html"><p>Módulo 1</p></a>
+                      <a href="../../vistas/modulo1/animacion1.html"><p>Módulo 1</p></a>
                       <i class="estado-modulo fs-5"></i>
                   </div>
                   <div id="modulo2" class="d-flex justify-content-between align-items-center">
-                      <a href="./vistas/modulo2/animacion2.html"><p>Módulo 2</p></a>
+                      <a href="../../vistas/modulo2/animacion2.html"><p>Módulo 2</p></a>
                       <i class="estado-modulo fs-5"></i>
                   </div>
                   <div id="modulo3" class="d-flex justify-content-between align-items-center">
-                      <a href="./vistas/modulo3/animacion3.html"><p>Módulo 3</p></a>
+                      <a href="../../vistas/modulo3/animacion3.html"><p>Módulo 3</p></a>
                       <i class="estado-modulo fs-5"></i>
                   </div>
                   <div id="modulo4" class="d-flex justify-content-between align-items-center">
-                      <a href="./vistas/modulo4/animacion4.html"><p>Módulo 4</p></a>
+                      <a href="../../vistas/modulo4/animacion4.html"><p>Módulo 4</p></a>
                       <i class="estado-modulo fs-5"></i>
                   </div>
               </li>
